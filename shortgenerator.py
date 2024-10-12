2e7adbf7fe63cf7d1e156a70de495ddc0a11ee8b
Dom <domkkaaaa@gmail.com>
2024-10-12 14:28:11 +0200

    Get shortest Clip instead random clip, Deletion of clips not working anymore

--- shortgenerator.py
+++ shortgenerator.py
@@ -3,6 +3,16 @@ import re
 import random
 from moviepy.editor import *
 
+def get_shortest_clip(videolist: list, path):
+    min_clip = VideoFileClip(path + videolist[0],target_resolution=(1920,1080))
+    min_index = 0
+    for i, video in enumerate(videolist):
+        clip = VideoFileClip(path + video,target_resolution=(1920,1080))
+        if clip.duration < min_clip.duration:
+            min_clip = clip
+            min_index = i
+    return min_clip, min_index
+
 def shortgenerator():
     path = "G:/videos/"
     pathused = "G:/videos/used/"
@@ -23,30 +33,29 @@ def shortgenerator():
     if len(videolist) == 0:
         exit("no videos found")
 
-    random.shuffle(videolist)
-
     #append clips until video length reached
     while duration <= shortlength:
         if len(videolist) == 0:
             break
-        clip = VideoFileClip(path + videolist[0],target_resolution=(1920,1080))
+        clip, min_index = get_shortest_clip(videolist, path)
         clips.append(clip)
-        usedVideos.append(videolist.pop(0))
+        usedVideos.append(videolist.pop(min_index))
         duration = 0
         for clip in clips:
             duration += clip.duration
 
+    #name file
     counter = 0
     while filename in os.listdir(output_path):
         counter += 1
         filename = f"{shortname}{counter}.mp4"
 
+    #concat clips add text and write file
     concat_clip = concatenate_videoclips(clips=clips, method="compose" )
     txt_clip = TextClip("gagreflexlol", fontsize=75, color="white").set_position(("center",0.7), relative=True).set_duration(concat_clip.duration)
     clip = CompositeVideoClip(bg_color=(0,0,0), clips=[concat_clip,txt_clip])
-
     clip.write_videofile(output_path + filename,threads=8, fps=30)
 
-
-    for usedVideo in usedVideos:
-        os.remove(path + usedVideo)
+    #delete usedVideos  // not working anymore because clip open in different Process :(
+    #for usedVideo in usedVideos:
+    #    os.remove(path + usedVideo)
